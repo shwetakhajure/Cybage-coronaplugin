21bc2af5451a074eb3211590ef8af5614911dad1
shweta khajure <shwetakha@cybage.com>
2017-09-04 15:13:52 +0530

    commented uploading to hockeyapp

--- ios/build.sh
+++ ios/build.sh
@@ -27,4 +27,4 @@ xcodebuild -target VungleCoronaTest -project VungleCoronaTest.xcodeproj clean
 xcodebuild -project VungleCoronaTest.xcodeproj -scheme VungleCoronaTest archive -archivePath ./VungleCoronaTest.xcarchive
 xcodebuild -exportArchive -exportFormat ipa -archivePath "./VungleCoronaTest.xcarchive/" -exportPath "./VungleCoronaTest.ipa" -exportProvisioningProfile "Vungle In House Distribution"
 
-puck -api_token=d6cb4cec883a44a5a39a0ed21a845ff3 -app_id=a63c146c01e7fd8eeebe15fad3dfc269 -submit=auto -download=true -notify=false -open=nothing VungleCoronaTest.ipa
+#puck -api_token=d6cb4cec883a44a5a39a0ed21a845ff3 -app_id=a63c146c01e7fd8eeebe15fad3dfc269 -submit=auto -download=true -notify=false -open=nothing VungleCoronaTest.ipa
