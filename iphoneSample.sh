c6c2112dcb4619ad8e77cc119475e60b5c56d3b4
shweta khajure <shwetakha@cybage.com>
2017-09-04 15:06:24 +0530

    commented clone of corona-plugin(line 5)

--- iphoneSample.sh
+++ iphoneSample.sh
@@ -2,7 +2,7 @@ set -ve
 cd ..
 [ -d ./corona-test-app ] && rm -rf ./corona-test-app
 [ -d ./Corona-Plugin ] && rm -rf ./Corona-Plugin
-git clone git@github.com:Vungle/Corona-Plugin.git
+#git clone git@github.com:Vungle/Corona-Plugin.git
 mkdir corona-test-app
 pushd corona-test-app
 mkdir app
