b180721fbc83b98323b340341668002aaa69731c
shweta khajure <shwetakha@cybage.com>
2017-09-04 18:43:01 +0530

    commented for temporary purpose( line 4,5)

--- androidBuild.sh
+++ androidBuild.sh
@@ -1,8 +1,8 @@
 set -ve
 pwd
 cd ..
-[ -d ./corona-test-app ] && rm -rf ./corona-test-app
-git clone -b sdk5 git@github.com:Vungle/corona-test-app.git
+#[ -d ./corona-test-app ] && rm -rf ./corona-test-app
+#git clone -b sdk5 git@github.com:Vungle/corona-test-app.git
 cd coronaplugin
 cd android
 ./build.sh ~/Library/Android/sdk/
