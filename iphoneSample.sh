96e45894695c253733c32b95589f7fbdd79039de
shweta khajure <shwetakha@cybage.com>
2017-09-04 15:29:45 +0530

    line 3,4 are not required any more

--- iphoneSample.sh
+++ iphoneSample.sh
@@ -1,7 +1,7 @@
 set -ve
 cd ..
-[ -d ./corona-test-app ] && rm -rf ./corona-test-app
-[ -d ./Corona-Plugin ] && rm -rf ./Corona-Plugin
+#[ -d ./corona-test-app ] && rm -rf ./corona-test-app
+#[ -d ./Corona-Plugin ] && rm -rf ./Corona-Plugin
 #git clone git@github.com:Vungle/Corona-Plugin.git
 mkdir corona-test-app
 pushd corona-test-app
