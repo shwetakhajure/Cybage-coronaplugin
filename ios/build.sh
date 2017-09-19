0affd46491b009d51403091adaa2a6b49c426d21
shweta khajure <shwetakha@cybage.com>
2017-09-19 18:46:51 +0530

    remving ENABLE_BITCODE

--- ios/build.sh
+++ ios/build.sh
@@ -23,8 +23,8 @@ rm Plugin/VungleAds.mm.bak
 
 [ -f ./VungleCoronaTest.ipa ] && rm ./VungleCoronaTest.ipa
 
-xcodebuild -target VungleCoronaTest -project VungleCoronaTest.xcodeproj clean build ENABLE_BITCODE=NO OTHER_LDFLAGS="-all_load -ObjC -lz -lobjc -lsqlite3 -lc++"
-xcodebuild -project VungleCoronaTest.xcodeproj -scheme VungleCoronaTest ENABLE_BITCODE=NO OTHER_LDFLAGS="-all_load -ObjC -lz -lobjc -lsqlite3 -lc++" archive -archivePath ./VungleCoronaTest.xcarchive 
+xcodebuild -target VungleCoronaTest -project VungleCoronaTest.xcodeproj clean build OTHER_LDFLAGS="-all_load -ObjC -lz -lobjc -lsqlite3 -lc++"
+xcodebuild -project VungleCoronaTest.xcodeproj -scheme VungleCoronaTest OTHER_LDFLAGS="-all_load -ObjC -lz -lobjc -lsqlite3 -lc++" archive -archivePath ./VungleCoronaTest.xcarchive 
 xcodebuild -exportArchive -exportFormat ipa -archivePath "./VungleCoronaTest.xcarchive/" -exportPath "./VungleCoronaTest.ipa" -exportProvisioningProfile "Vungle In House Distribution"
 
 #puck -api_token=d6cb4cec883a44a5a39a0ed21a845ff3 -app_id=a63c146c01e7fd8eeebe15fad3dfc269 -submit=auto -download=true -notify=false -open=nothing VungleCoronaTest.ipa
