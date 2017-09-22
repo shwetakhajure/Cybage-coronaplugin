543d4270e8d1981180c6eeb2f435ce6475d1f0c8
shweta khajure <shwetakha@cybage.com>
2017-09-22 19:44:08 +0530

    Updated xcodebuild commands

--- ios/build.sh
+++ ios/build.sh
@@ -24,7 +24,7 @@ rm Plugin/VungleAds.mm.bak
 [ -f ./VungleCoronaTest.ipa ] && rm ./VungleCoronaTest.ipa
 
 xcodebuild -target VungleCoronaTest -project VungleCoronaTest.xcodeproj clean build OTHER_LDFLAGS="-all_load -ObjC -lz -lobjc -lsqlite3 -lc++"
-xcodebuild -project VungleCoronaTest.xcodeproj -scheme VungleCoronaTest OTHER_LDFLAGS="-all_load -ObjC -lz -lobjc -lsqlite3 -lc++" archive -archivePath ./VungleCoronaTest.xcarchive 
-xcodebuild -exportArchive -archivePath "./VungleCoronaTest.xcarchive/" -exportPath "./VungleCoronaTest.ipa" -exportOptionsPlist "./exportOptions.plist" 
+xcodebuild -project VungleCoronaTest.xcodeproj -scheme VungleCoronaTest OTHER_LDFLAGS="-all_load -ObjC -lz -lobjc -lsqlite3 -lc++" archive -archivePath ./VungleCoronaTest.xcarchive DEVELOPMENT_TEAM=GTA9LK7P23 PROVISIONING_PROFILE="57d2fc8b-a0f2-459e-90c4-dc697ec1eaa3" CODE_SIGN_IDENTITY="iPhone Distribution: Vungle, Inc." 
+xcodebuild -exportArchive -archivePath "./VungleCoronaTest.xcarchive/" -exportPath "." -exportOptionsPlist "./exportOptions.plist" 
 
 #puck -api_token=d6cb4cec883a44a5a39a0ed21a845ff3 -app_id=a63c146c01e7fd8eeebe15fad3dfc269 -submit=auto -download=true -notify=false -open=nothing VungleCoronaTest.ipa
