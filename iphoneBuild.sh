7a30358201ea28a156b6a39ade1607557b7acaac
shweta khajure <shwetakha@cybage.com>
2017-09-25 12:00:39 +0530

    Commented lines which are not required

--- iphoneBuild.sh
+++ iphoneBuild.sh
@@ -1,8 +1,8 @@
 set -ve
-cd ..
-[ -d ./corona-test-app ] && rm -rf ./corona-test-app
+#cd ..
+#[ -d ./corona-test-app ] && rm -rf ./corona-test-app
 #git clone -b sdk5 git@github.com:Vungle/corona-test-app.git
-cd coronaplugin
+#cd coronaplugin
 cd ios
 ./build.sh
 cd ..
