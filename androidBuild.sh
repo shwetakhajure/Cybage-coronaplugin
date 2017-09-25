9b6ee03fe30b5c788593396391b899eec73e1e08
shweta khajure <shwetakha@cybage.com>
2017-09-25 12:08:18 +0530

    Commented lines which are not required.

--- androidBuild.sh
+++ androidBuild.sh
@@ -1,10 +1,10 @@
 set -ve
 pwd
-cd ..
+#cd ..
 #[ -d ./corona-test-app ] && rm -rf ./corona-test-app
 #git clone -b sdk5 git@github.com:Vungle/corona-test-app.git
-cd coronaplugin
+#cd coronaplugin
 cd android
 ./build.sh ~/Desktop/Android/sdk/
 cd ..
-[ -d ../corona-test-app ] && rm -rf ../corona-test-app
+#[ -d ../corona-test-app ] && rm -rf ../corona-test-app
